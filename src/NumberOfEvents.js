5f0b4ab5c481ab47a34067b52f1e5eb06e08889f
Polja137 <117524191+Polja137@users.noreply.github.com>
2023-05-25 17:09:51 +0200

    alerts implemention

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-  state = { noe: 32 }
+  state = { noe: 32,
+            showError: false // Add a new state property to track error visibility
+          }
 
   componentDidMount() {
     this.setState({ noe: this.props.noe || 32 });
   }
 
   changeNOE(value) {
-    this.setState({ noe: value });
-    this.props.updateEvents(null, value);
+    if (value <= 0 || value > 100) { // Check if the value is invalid
+      this.setState({ showError: true }); // Show the error alert
+    } else {
+      this.setState({ noe: value, showError: false }); // Hide the error alert
+      this.props.updateEvents(null, value);
+    }
   }
-
+  
   render() {
-    const { noe } = this.state;
+    const { noe, showError } = this.state;
     return (
-    <div className="NumberOfEvents">
+      <div className="NumberOfEvents">
         <h3>Number of Events:</h3>
         <input
           className="noe-input"
           type="number"
           value={noe}
-          onChange={event => {this.changeNOE(event.target.value);}}
-        >
-        </input>
-    </div>
-    )
+          onChange={(event) => {
+            this.changeNOE(event.target.value);
+          }}
+        ></input>
+        {showError && <ErrorAlert text="Invalid number of events" />} {/* Render the error alert if showError is true */}
+      </div>
+    );
   }
+ 
 
 }
 
